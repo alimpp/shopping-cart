bbd2caf4a9b021e5ba3dbbf54475c9ca473ec093
ali.mohsenpour <alimpp766@gmail.com>
2022-06-08 13:47:29 -0700

    make project

--- src/main.js
+++ src/main.js
@@ -4,4 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import './assets/css/main.css'
+import './assets/css/anim.css'
+import './assets/css/size.css'
+import './assets/css/text-color.css'
+
 createApp(App).use(store).use(router).mount('#app')

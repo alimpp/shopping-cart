3f0f7edabaca7442d567633800c317ad4609b983
ali.mohsenpour <alimpp766@gmail.com>
2022-06-09 15:15:11 -0700

    fix home page

--- src/components/home/homeDC.vue
+++ src/components/home/homeDC.vue
@@ -11,29 +11,23 @@
                   <p class="dark_color size_c">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit in tempora, voluptates aut nobis numquam. Nisi corrupti delectus ipsum reiciendis rem!</p>
                   <coreBtnPrimary @click="goToHome" :getStarted="getStarted" />
               </div>
-          </div>
+           </div>
 
-          <div class="row animate__animated animate__fadeIn" ref="home">
-              <div class="col-sm-4" v-for="data in getData" :key="data.id">
-                   <div class="card mb-3" style="max-width: 540px;">
-                         <div class="row g-0">
-                        <div class="col-md-4">
-                        <img :src="data.img" class="img-fluid rounded-start" alt="ERROR" style="width:100%;">
-                        </div>
-                        <div class="col-md-8">
-                        <div class="card-body">
-                            <p class="dark_color size_b">{{data.name}}</p>
-                            <p class="dark_color size_z">{{data.title}}</p>
-                            <router-link to="/product" class="btn btn-primary">Add To Cart</router-link>
-                        </div>
-                        </div>
-                        </div>
+            <div class="row my-5 animate__animated animate__fadeIn" ref="home">
+              <div class="col-sm-6 justify">
+                  <p class="dark_color size_b">Lorem ipsum dolor...</p>
+                  <p class="dark_color size_c">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit in tempora, voluptates aut nobis numquam. Nisi corrupti delectus ipsum reiciendis rem!</p>
+                  <div>
+                       <router-link to="/product" class="btn btn-secondary">Go To Product</router-link>
                   </div>
-             </div>
-          </div>
+              </div>
+              <div class="col-sm-6">
+                  <img src="./img/shopp.jpg" class="img animate__animated animate__fadeIn" alt="error">
+              </div>
+           </div>
 
          <coreFooter/>
-         
+
       </div>
   </div>
 </template>
@@ -48,7 +42,7 @@ export default {
            getStarted : "Get Started" ,
        }
    } ,
-   components : {coreBtnPrimary , coreFooter} ,
+   components : {coreBtnPrimary , coreFooter } ,
    computed : {
        getData(){
           return this.$store.getters['Products/filterProduct']

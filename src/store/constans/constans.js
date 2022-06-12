e073046d1274261ecd7c953bc6c8f12e4560ddae
ali.mpp <alimpp766@gmail.com>
2022-06-12 14:12:33 +0430

    Make Login In Store

---
+++ src/store/constans/constans.js
@@ -0,0 +1,3 @@
+export const LOGIN = "LOGIN"
+export const ONSTART = "ONSTART"
+export const LOGOUT = "LOGOUT"
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,43 @@
 import { createStore } from 'vuex'
 import Products from './modules/product'
-
+import { LOGIN , ONSTART,  LOGOUT} from './constans/constans'
 export default createStore({
   state: {
+    isAuth : false , 
+    token : "" , 
   },
   mutations: {
-  },
-  actions: {
+    [ONSTART](state){
+      let token = localStorage.getItem("token")
+      if(token){
+          state.isAuth = true
+          state.token = token
+          axios.defaults.headers.common['Authorization'] = "Token " + token
+      }else{
+          state.isAuth = false
+          state.token = ""
+          axios.defaults.headers.common['Authorization'] = ""
+      }
+    } , 
+    [LOGIN](state , token){
+      if(token){
+          state.isAuth = true
+          state.token = token 
+          localStorage.setItem("token" , token)
+         axios.defaults.headers.common['Authorization'] = "Token " + token
+      }else{
+          state.isAuth = false 
+          state.token = ""
+          localStorage.removeItem('token')
+          axios.defaults.headers.common['Authorization'] = ""
+      }
+    } , 
+    [LOGOUT](state){
+        state.isAuth = false 
+        state.token = ""
+        axios.defaults.headers.common['Authorization'] = ""
+        localStorage.removeItem('token')
+      }
   },
   modules: {
     Products : Products

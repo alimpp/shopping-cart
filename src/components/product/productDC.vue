0a7a84deac503c74bcbc0032a7f0ef775dbaf805
ali.mohsenpour <alimpp766@gmail.com>
2022-06-14 07:06:25 -0700

    make change in single Route

--- src/components/product/productDC.vue
+++ src/components/product/productDC.vue
@@ -3,16 +3,21 @@
       <div class="container animate__animated animate__fadeIn">
           <div class="row">
               <div class="col-sm-4" v-for="data in getData" :key="data.id">
-                <a class="link" :href="`/product/${data.id}`">
+                
                     <div class="card mb-3 cardHover" style="max-width: 540px;">
                     <div class="row g-0">
                         <div class="col-md-4">
+                        <a class="link" :href="`/product/${data.id}`">
                         <img :src="data.img" class="img-fluid rounded-start" alt="ERROR" style="width:100%;">
+                        </a>
                         </div>
                         <div class="col-md-8">
                         <div class="card-body">
-                            <p class="dark_color size_b">{{data.name}}</p>
+                            <a class="link" :href="`/product/${data.id}`"><p class="dark_color size_b">{{data.name}}</p></a>
+                            <a class="link" :href="`/product/${data.id}`">
                             <p class="dark_color size_x">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer</p>
+                            </a>
+                            <a class="link" :href="`/product/${data.id}`">
                             <div class="d-flex">
                             <p class="dark_color size_b">{{data.price}}</p>
                             <div class="d-flex mt-2 mx-2">
@@ -23,12 +28,13 @@
                                 <i class="bi bi-star-fill" style="color:yellow;"></i>
                             </div>
                             </div>
+                            </a>
                             <coreBtnPrimary class="mt-2" :addToCart="addToCart"/>
                         </div>
                         </div>
                     </div>
                     </div>
-                </a>
+               
               </div>
           </div>
           <router-view></router-view>
